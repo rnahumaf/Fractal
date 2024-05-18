428a76dd99742a0727276fed97fc3e8f7d5bd831
Rodrigo Nahum <34604938+rnahumaf@users.noreply.github.com>
2024-05-18 17:54:35 -0300

    Update and rename Circular_cartesian.R to Slow_CentripetoCirculo_CentroDeMassa.R

--- Slow_CentripetoCirculo_CentroDeMassa.R
+++ Slow_CentripetoCirculo_CentroDeMassa.R
@@ -1,13 +1,14 @@
 # Configurações iniciais
 D <- 500          # Diâmetro do círculo
-loops <- 15000    # Número de pontos no círculo
+loops <- 15000    # Número de pontos a serem adicionados no círculo
 passo <- 1        # Distância percorrida por cada passo do ponto em direção ao centro gravitacional
 
 # Inicialização da matriz e variáveis
-m <- matrix(rep(0, D^2), nrow = D)   # Matriz principal
+m <- matrix(0, nrow = D, ncol = D)  # Matriz principal
 r <- D * 0.4                        # Raio do círculo
-atual_i <- D / 2                    # Coordenada inicial i (centro do círculo)
-atual_j <- D / 2                    # Coordenada inicial j (centro do círculo)
+centro <- D / 2                     # Coordenada do centro do círculo
+atual_i <- centro                   # Coordenada inicial i (centro do círculo)
+atual_j <- centro                   # Coordenada inicial j (centro do círculo)
 m[atual_i, atual_j] <- 1            # Marca o ponto inicial no centro do círculo
 vetor_i <- numeric(loops)           # Vetor para armazenar coordenadas i dos pontos
 vetor_j <- numeric(loops)           # Vetor para armazenar coordenadas j dos pontos
@@ -22,22 +23,25 @@ for (counts in 1:loops) {
   posic <- runif(1) * 2
   
   # Calcula a posição relativa do ponto em relação ao centro do círculo
-  atual_i <- D / 2 - sinpi(posic) * r
-  atual_j <- D / 2 - cospi(posic) * r
+  atual_i <- centro - sinpi(posic) * r
+  atual_j <- centro - cospi(posic) * r
   
   # Loop interno para mover o ponto em direção ao centro de massa
   repeat {
-    # Calcula as novas coordenadas do ponto após dar um passo em direção ao centro de massa
-    centro_i <- sum(vetor_i) / length(vetor_i)
-    centro_j <- sum(vetor_j) / length(vetor_j)
+    # Calcula o centro de massa atual
+    centro_i <- mean(vetor_i[1:counts])
+    centro_j <- mean(vetor_j[1:counts])
+    
+    # Calcula a distância do ponto atual ao centro de massa
     distancia <- sqrt((centro_j - atual_j)^2 + (centro_i - atual_i)^2)
     
+    # Dá um passo em direção ao centro de massa
     atual_i <- atual_i + (centro_i - atual_i) * passo / distancia
     atual_j <- atual_j + (centro_j - atual_j) * passo / distancia
     
     # Verifica se o ponto chegou próximo a um ponto adjacente e marca na matriz principal
-    if (any(m[(atual_i - 1):(atual_i + 1), (atual_j - 1):(atual_j + 1)] == 1)) {
-      m[atual_i, atual_j] <- 1
+    if (any(m[(round(atual_i) - 1):(round(atual_i) + 1), (round(atual_j) - 1):(round(atual_j) + 1)] == 1)) {
+      m[round(atual_i), round(atual_j)] <- 1
       break
     }
   }
